373b829579cd0b729b54dc7a1e171b853df3c09a
Ranmdom <slimegamer12@gmail.com>
2025-03-24 22:47:09 -0300

    Correção do arquvio

--- components/costumer-login-form.tsx
+++ components/costumer-login-form.tsx
@@ -9,7 +9,7 @@ import { Eye, EyeOff, Loader2 } from "lucide-react"
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
-import { useAuth } from "@/context/auth-context"
+import { useAuth } from "../context/auth-context"
 import { toast } from "@/hooks/use-toast"
 
 export default function CustomerLoginForm() {
